6bf4b967528857737342fa3c8324dd6044be4de4
treypisano <treypisano@treypc>
2023-05-15 17:23:29 -0400

    add border color to invalid name

--- react-form/src/form.jsx
+++ react-form/src/form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./form.css"
 
 const Form = (props) => {
   const [name, setName] = useState("");
@@ -15,6 +16,8 @@ const Form = (props) => {
     const specialChars = ["!", "@", "#", "$", "%", "."];
     let errors = [];
     if (name.length === 0) {
+      const nameElement = document.querySelector("#name")
+      nameElement.className = "notValidated"
       errors.push("name can't be empty");
     }
     if (bio.length > 280) {
@@ -54,13 +57,13 @@ const Form = (props) => {
           setBio(e.target.value);
           break;
         case "phoneType": 
-          setphoneType(e.target.value);
+          setphoneType(e.target.value)
           break;
         case "staff":
           setStaff(e.target.value);
           break;
         case "emailNotification":
-          setEmailNotification(e.target.value);
+          setEmailNotification(e.target.checked);
           break;
         default:
           break;
@@ -82,7 +85,8 @@ const Form = (props) => {
         phoneType,
         bio,
         staff,
-        emailNotification
+        emailNotification,
+        "date": new Date()
       };
       console.log(user);
     }
@@ -107,48 +111,57 @@ const Form = (props) => {
       {showError()}
       <form onSubmit={handleSubmit}>
         <input
+          id="name"
           type="text"
           placeholder="name"
           value={name}
           onChange={handleChange("name")}
         />
+        <br />
         <input
+          id="email"
           type="text"
           placeholder="example@mail.com"
           value={email}
           onChange={handleChange("email")}
         />
+        <br />
         <input
+          id="phoneNumber"
           type="text"
           placeholder="XXX-XXX-XXXX(ONLY AMERICAN!!!)"
           value={phoneNumber}
           onChange={handleChange("phoneNumber")}
         />
+        <br />
+
         <select name="phoneType" 
         id="phoneType"
         onChange={handleChange("phoneType")}
-        required>
+        required disabled={phoneNumber === ""}>
           <option></option>
           <option value="Home">Home</option>
           <option value="Work">Work</option>
           <option value="Mobile">Mobile</option>
         </select>
+        <br />
         <textarea
           //   type="textarea"
           placeholder="bio"
           value={bio}
           onChange={handleChange("bio")}
         />
-
+        <br />
         <input type="radio" name="staff" id="student" value="student" onChange={handleChange("staff")} required/>
         <label htmlFor="student">Student</label>
         <input type="radio" name="staff" id="Instructor" value="Instructor" onChange={handleChange("staff")} required/>
         <label htmlFor="Instructor">Instructor</label>
-
+        <br />
         <input type="checkbox" onChange ={handleChange("emailNotification")} />
         Sign up for email notifications?
         <button>submit</button>
       </form>
+      {/* let nameId = document.getElementById("name") */}
     </>
   );
 };
